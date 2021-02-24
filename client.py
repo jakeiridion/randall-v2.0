94939c24c316de0635ecfa09f10f76ab7a98ae8c
jakeiridion <emil.ej.jeleskovic@gmail.com>
2021-02-24 10:00:27 +0100

    buffer in camera class not in client class.

--- client.py
+++ client.py
@@ -13,10 +13,11 @@ class Capture:
 
         self.__height, self.__width = resolution
         # frame.shape = (height, width, 3)
-        self.__frame = np.empty((self.__height, self.__width, 3))
+
+        self.__buffer = Queue()
 
     def get_frame(self):
-        return self.__frame
+        return self.__buffer.get()
 
     def is_running(self):
         return self.__is_running
@@ -30,7 +31,7 @@ class Capture:
                 ret, frame = cap.read()
                 frame = cv2.resize(frame, (self.__width, self.__height))
                 frame = cv2.flip(frame, 1)
-                self.__frame = frame
+                self.__buffer.put(frame)
             cap.release()
 
         Thread(target=loop, daemon=True).start()
@@ -41,12 +42,6 @@ class Capture:
 
 class Client:
     def __init__(self):
-        self.__buffer = Queue()
-
-        # TODO: make .conf with custom resolution option
-        self.__use_custom_resolution = False
-        self.__resolution = None
-
         self.__ip = socket.gethostbyname(socket.gethostname())  # TODO: change with netifaces
         self.__port = 5050
         self.__identifier = b"c"  # camera
@@ -55,21 +50,44 @@ class Client:
         self.__management_connection.setblocking(True)
         self.__initialize_management_connection()
 
+        self.__buffer = Queue()
+
+        self.__use_custom_resolution = False
+        self.__height, self.__width = self.__request_resolution()
+        self.__frame_byte_length = self.__height * self.__width * 3
+
         self.__udp_connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.__udp_connection.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, self.__frame_byte_length * 4)
+
+        self.dtype = np.dtype(np.uint8)
 
     def __initialize_management_connection(self):
         self.__management_connection.connect((self.__ip, self.__port))
         self.__management_connection.send(self.__identifier)
-        self.__request_resolution()
 
     def __request_resolution(self):
         if self.__use_custom_resolution is False:
-            self.__management_connection.send(b"r")  # request resolution
+            self.__management_connection.send(b"gr")  # get resolution
             resolution = struct.unpack(">2H", self.__management_connection.recv(struct.calcsize(">2H")))
-            self.__resolution = resolution
-            print(resolution)
+            height, width = resolution
         else:
-            self.__resolution = (480, 640)
+            # TODO: make .conf with custom resolution option
+            height, width = (480, 640)
+            self.__management_connection.send(b"sr")  # set resolution
+            self.__management_connection.send(struct.pack(">2H", height, width))
+        print(height, width)
+        return height, width
+
+    def get_resolution(self):
+        return self.__height, self.__width
 
 
-client = Client()
+if __name__ == '__main__':
+    client = Client()
+    capture = Capture(client.get_resolution())
+    capture.start()
+
+    while capture.is_running():
+        frame = capture.get_frame()
+        cv2.waitKey(1)
+        cv2.imshow("frame", frame)
--- server.py
+++ server.py
@@ -6,19 +6,34 @@ import socket
 from queue import Queue
 
 
+class Camera:
+    def __init__(self, connection, height, width):
+        self.management_connection = connection
+        self.frame_chunks = Queue()
+        self.frames = Queue()
+        self.height = height
+        self.width = width
+
+
 class Server:
     def __init__(self):
         self.__cameras = {}
 
         self.__height = 480
         self.__width = 640
+        self.__frame_byte_length = self.__height * self.__width * 3
 
         self.__ip = socket.gethostbyname(socket.gethostname())
         self.__port = 5050
 
         self.__management_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__management_connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
         self.__management_connection.setblocking(True)
         self.__handle_new_management_connections()
+
+        self.__udp_connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # TODO: frame byte length changes depending on camera.
+        self.__udp_connection.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, self.__frame_byte_length*4)
 
     def __handle_new_management_connections(self):
         self.__management_connection.bind((self.__ip, self.__port))
@@ -33,25 +48,38 @@ class Server:
         Thread(target=loop, daemon=True).start()
 
     def __handle_identifier(self, identifier, connection, ip):
-        if identifier == b"c":
-            # cameras[ip] = (identifier, management_connection_to_camera, chunk_queue, frame_queue)
-            self.__cameras[ip] = (identifier, connection, Queue(), Queue())
+        if identifier == b"c":  # camera
+            self.__cameras[ip] = Camera(connection, self.__height, self.__width)
             self.__handle_existing_management_connection(ip)
         else:
             connection.close()
 
     def __handle_existing_management_connection(self, ip):
         def loop():
-            conn = self.__cameras[ip][1]
+            conn = self.__cameras[ip].management_connection
             while True:
-                request = conn.recv(1)
-                if request == b"r":  # request for resolution
+                request = conn.recv(2)
+                if request == b"gr":  # get resolution
                     conn.send(struct.pack(">2H", self.__height, self.__width))
+                elif request == b"sr":  # set resolution
+                    resolution = struct.unpack(">2H", conn.recv(struct.calcsize(">2H")))
+                    self.__cameras[ip].height, self.__cameras[ip].width = resolution
+                elif request == b"ex":  # exit
+                    conn.close()
+                    del self.__cameras[ip]
+                elif request == struct.pack(">?", True):
+                    # start sending frames
+                    pass
+                elif request == struct.pack(">?", False):
+                    # stop sending frames
+                    pass
+
                 # TODO: fill other possible requests
 
         Thread(target=loop, daemon=True).start()
 
 
-server = Server()
-while True:
-    pass
+if __name__ == '__main__':
+    server = Server()
+    while True:
+        pass
