e6f100233b759ec49c56b34a0f882df5e25c5bdd
jakeiridion <emil.ej.jeleskovic@gmail.com>
2021-08-03 19:34:24 +0200

    fixed double logging bug.

--- src/shared/Logger.py
+++ src/shared/Logger.py
@@ -3,19 +3,21 @@ from logging.handlers import RotatingFileHandler
 import os
 
 
-def __initiate_logger(name, log_path):
+def __initiate_logger(name, log_path, debug):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
-    file_handler = RotatingFileHandler(log_path, maxBytes=10_000_000, backupCount=1)
-    file_handler.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s:%(name)s:%(levelname)s:%(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+    if not logger.handlers:
+        file_handler = RotatingFileHandler(log_path, maxBytes=10_000_000, backupCount=1)
+        file_handler.setLevel(logging.INFO)
+        formatter = logging.Formatter("%(asctime)s:%(name)s:%(levelname)s:%(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        if debug:
+            logger = __make_debug_logger(logger, formatter)
     return logger
 
 
-def __make_debug_logger(logger):
-    formatter = logging.Formatter("%(asctime)s:%(name)s:%(levelname)s:%(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+def __make_debug_logger(logger, formatter):
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.DEBUG)
     stream_handler.setFormatter(formatter)
@@ -33,7 +35,5 @@ def create_logger(name, debug, log_filename):
     log_path = os.path.join(root_path, "logs")
     log_filename = os.path.join(log_path, log_filename)
     __create_log_folder_if_necessary(log_path)
-    logger = __initiate_logger(name, log_filename)
-    if debug:
-        logger = __make_debug_logger(logger)
+    logger = __initiate_logger(name, log_filename, debug)
     return logger
